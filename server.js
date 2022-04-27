e453bca3436740dbf04fb8a97ef7af9434bb24ea
Jake Walsh <jkwalsh127@gmail.com>
2022-04-27 09:20:10 -0700

    add route to database in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 // const api = require('./public/scripts/index.js');
 
-// const dbData = require('./db/db.json');
+const dbData = require('./db/db.json');
 const app = express();
 const PORT = 3005;
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => res.send('Navigate to ...'));
 // app.get('/', (req, res) =>
 //     res.sendFile(path.join(__dirname, '/public/index.html'))
 // );
-// app.get('/api/db', (req, res) => res.json(dbData));
+app.get('/api/db', (req, res) => res.json(dbData));
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
