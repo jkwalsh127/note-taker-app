1d1c3ab123cdae7fcb6e3923e049fde71a666602
Jake Walsh <jkwalsh127@gmail.com>
2022-04-27 21:11:27 -0700

    bring notes.js online again

--- routes/notes.js
+++ routes/notes.js
@@ -2,35 +2,35 @@ const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
-// notes.get('/', (req, res) => {
-//     readFromFile('./db/db.json', "utf-8").then((data) => {
-//         res.json(JSON.parse(data));
-//     }).catch((error) => {
-//         console.error(`Unable to satisfy ${req.method} request`);
-//         console.error(error);
-//         res.json("unable to satisfy request")
-//     })
-// });
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json', "utf-8").then((data) => {
+        res.json(JSON.parse(data));
+    }).catch((error) => {
+        console.error(`Unable to satisfy ${req.method} request`);
+        console.error(error);
+        res.json("unable to satisfy request")
+    })
+});
 
-// notes.post('/', (req, res) => {
+notes.post('/', (req, res) => {
 
-//     const { title, text } = req.body;
+    const { title, text } = req.body;
 
-//         const newNote = {
-//             title,
-//             text,
-//             // title: "helooo",
-//             note_id: uuid(),
-//         };
+        const newNote = {
+            title,
+            text,
+            // title: "helooo",
+            note_id: uuid(),
+        };
 
-//         readAndAppend(newNote, './db/db.json');
+        readAndAppend(newNote, './db/db.json');
 
-//         const response = {
-//             status: 'success',
-//             body: newNote,
-//         };
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
 
-//         res.json(response);
-// });
+        res.json(response);
+});
 
 module.exports = notes;
